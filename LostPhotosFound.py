f4d85b5f3b0292e3ab9296c849bded4aa9579671
Caio Begotti <caio1982@gmail.com>
2012-10-25 15:55:45 -0200

    thanks @coxande

--- LostPhotosFound.py
+++ LostPhotosFound.py
@@ -12,6 +12,7 @@
 #	- create a simple interface with pyqt like an album frame (QFileSystemWatcher?)
 #	- CLI options for host, username, password, folder, criteria and attachment size
 #	- is_multipart() seems better if attachment is too big/inline (it crashes)
+#	- check if All Mail labels is visible before selecting it
 #	- better (real) debug logging :-)
 
 # to save the images
