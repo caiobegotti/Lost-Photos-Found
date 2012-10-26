14c4fed196376a7b6b29a7c0d994aafad6c494ee
Caio Begotti <caio1982@gmail.com>
2012-10-26 09:44:09 -0200

    all changes from the old scripts/ repository before any changes done by @cmatsuoka in his branch were merged in (actually he started working on those changes right after 35a3516bb1248371b2c1999135874b872ff401e7 but i sort of got lost here)

--- LostPhotosFound.py
+++ LostPhotosFound.py
@@ -27,79 +27,81 @@ from imapclient import IMAPClient
 HOST = 'imap.gmail.com'
 USERNAME = 'username@gmail.com'
 PASSWORD = 'password'
-ALL_MAIL = '[Gmail]/All Mail'
-SSL = True
 
-try:
-    server = IMAPClient(HOST, use_uid=True, ssl=SSL)
-except:
-    raise Exception('Could not successfully connect to the IMAP host')
+def get_server(host, username, password):
+    all_mail = '[Gmail]/All Mail'
+    try:
+        server = IMAPClient(host, use_uid=True, ssl=True)
+    except:
+        raise Exception('Could not successfully connect to the IMAP host')
 
-server.login(USERNAME, PASSWORD)
+    server.login(username, password)
 
-if not server.folder_exists(ALL_MAIL):
-    for folder in server.xlist_folders():
-        labels = folder[0]
-        if 'AllMail' in labels[-1]:
-            ALL_MAIL = folder[2]
+    if not server.folder_exists(all_mail):
+        for folder in server.xlist_folders():
+            labels = folder[0]
+            if 'AllMail' in labels[-1]:
+                all_mail = folder[2]
 
-server.select_folder(ALL_MAIL)
+    server.select_folder(all_mail)
+    return server
 
-    #print "Available folders:"
-    #for f in server.list_folders():
-    #    print f[-1]
-    #raise Exception('Your "All Mail" label is either not visible or in another language!')
+def get_messages(server):
+    # that's why we only support gmail
+    # for other mail services we'd have to translate the custom
+    # search to actual IMAP queries, thus no X-GM-RAW cookie to us
+    criteria = 'X-GM-RAW "has:attachment filename:(jpg OR jpeg OR gif OR png OR tiff OR tif OR ico OR xbm OR bmp)"'
+    messages = server.search([criteria])
 
-# that's why we only support gmail
-# for other mail services we'd have to translate the custom
-# search to actual IMAP queries, thus no X-GM-RAW cookie to us
-criteria = 'X-GM-RAW "has:attachment filename:(jpg OR jpeg OR gif OR png OR tiff OR tif OR ico OR xbm OR bmp)"'
-messages = server.search([criteria])
+    # stats
+    print 'LOG: %d messages matched the search criteria %s' % (len(messages), criteria)
+    return messages
 
-# stats
-print 'LOG: %d messages matched the search criteria %s' % (len(messages), criteria)
-
-for m in messages:
-    data = server.fetch([m], ['RFC822'])
-    for d in data:
-        # SEQ is also available
-        mail = message_from_string(data[d]['RFC822'])
-        if mail.get_content_maintype() != 'multipart':
-            continue
-        
-        # this whole mess up to the print statement
-        # is only for debugging purposes :-(
-        header_from = mail["From"]
-        if not decode_header(header_from).pop(0)[1]:
-            header_from = decode_header(header_from).pop(0)[0].decode('iso-8859-1').encode('utf-8')
-        else:
-            header_from = decode_header(header_from).pop(0)[0].decode(decode_header(header_from).pop(0)[1]).encode('utf-8')
-        
-        header_subject = mail["Subject"]
-        if not decode_header(header_subject).pop(0)[1]:
-            header_subject = decode_header(header_subject).pop(0)[0].decode('iso-8859-1').encode('utf-8')
-        else:
-            header_subject = decode_header(header_subject).pop(0)[0].decode(decode_header(header_subject).pop(0)[1]).encode('utf-8')
-        
-        print '[%s]: %s' % (header_from, header_subject)
-
-        for part in mail.walk():
-            if part.get_content_maintype() == 'multipart':
+def filter_content(messages):
+    for m in messages:
+        data = server.fetch([m], ['RFC822'])
+        for d in data:
+            # SEQ is also available
+            mail = message_from_string(data[d]['RFC822'])
+            if mail.get_content_maintype() != 'multipart':
                 continue
-            if part.get('Content-Disposition') is None:
-                continue
-            filename = part.get_filename()
-            seq = 1
-            if not filename:
-                filename = 'attachment-%03d%s' % (seq, 'bin')
-                seq += 1
-            if not os.path.isdir(USERNAME):
-                os.mkdir(USERNAME)
-            saved = os.path.join(USERNAME, filename)
-            if not os.path.isfile(saved):
-                f = open(saved, 'wb')
-                f.write(part.get_payload(decode=True))
-                f.close()
+            
+            # this whole mess up to the print statement
+            # is only for debugging purposes :-(
+            header_from = mail["From"]
+            if not decode_header(header_from).pop(0)[1]:
+                header_from = decode_header(header_from).pop(0)[0].decode('iso-8859-1').encode('utf-8')
+            else:
+                header_from = decode_header(header_from).pop(0)[0].decode(decode_header(header_from).pop(0)[1]).encode('utf-8')
+            
+            header_subject = mail["Subject"]
+            if not decode_header(header_subject).pop(0)[1]:
+                header_subject = decode_header(header_subject).pop(0)[0].decode('iso-8859-1').encode('utf-8')
+            else:
+                header_subject = decode_header(header_subject).pop(0)[0].decode(decode_header(header_subject).pop(0)[1]).encode('utf-8')
+            
+            print '[%s]: %s' % (header_from, header_subject)
+    
+            for part in mail.walk():
+                if part.get_content_maintype() == 'multipart':
+                    continue
+                if part.get('Content-Disposition') is None:
+                    continue
+                filename = part.get_filename()
+                seq = 1
+                if not filename:
+                    filename = 'attachment-%03d%s' % (seq, 'bin')
+                    seq += 1
+                if not os.path.isdir(USERNAME):
+                    os.mkdir(USERNAME)
+                saved = os.path.join(USERNAME, filename)
+                if not os.path.isfile(saved):
+                    f = open(saved, 'wb')
+                    f.write(part.get_payload(decode=True))
+                    f.close()
 
+server = get_server(HOST, USERNAME, PASSWORD)
+messages = get_messages(server)
+filter_content(messages)    
 server.close_folder()
 server.logout()
