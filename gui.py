45340f44c9848ee519077bdd134f89fcfc7a1178
Caio Begotti <caio1982@gmail.com>
2013-03-18 16:44:34 -0300

    totally hackish... i wont have time to write a proper gui for this anyway, i am just playing around

--- gui.py
+++ gui.py
@@ -11,9 +11,14 @@ from PyQt4 import QtGui
 
 from lostphotosfound.config import Config
 
-app = QtCore.QCoreApplication(sys.argv)
+app = QtGui.QApplication(sys.argv)
 
-def monitoredChange():
+def monitoredChange(path):
+    filelist = os.listdir(filepath)
+    filelist = filter(lambda x: not os.path.isdir(x), filelist)
+    newest = max(filelist, key=lambda x: os.path.getmtime(filepath + '/' + x))
+    newest = filepath + '/' + newest
+    pic.setPixmap(QtGui.QPixmap(newest))
     print 'ACK'
 
 config = Config()
@@ -24,10 +29,12 @@ monitoredPath = [filepath]
 fswatcher = QtCore.QFileSystemWatcher(monitoredPath)
 fswatcher.directoryChanged.connect(monitoredChange)
 
-filelist = os.listdir(filepath)
-filelist = filter(lambda x: not os.path.isdir(x), filelist)
-
-newest = max(filelist, key=lambda x: os.path.getmtime(filepath + '/' + x))
-newest = filepath + '/' + newest
+window = QtGui.QMainWindow()
+window.setGeometry(0, 0, 500, 500)
+pic = QtGui.QLabel(window)
+pic.setGeometry(0, 0, 500, 500)
+pic.setScaledContents(True)
+#pic.setPixmap(QtGui.QPixmap(newest))
+window.show()
 
 sys.exit(app.exec_())
